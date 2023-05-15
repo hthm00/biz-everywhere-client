1186c39911d64bc4b0638f988f25ca144338660f
Banhana <68533491+Banhana@users.noreply.github.com>
2023-05-14 18:10:58 -0700

    Added props to BusinessItems

--- src/home/containers/Home.js
+++ src/home/containers/Home.js
@@ -10,28 +10,41 @@ function Home() {
 	return (
 		<div>
 			<NavBar />
-
 			<div className="container flex-grow-1">
 				<div className="p-4 container">
-					<Row>
-						<Col lg={8} md={6}>
-							<div className="d-flex align-items-center">
-								<h3 className="text-decoration-none text-secondary">
-									Recommended For You
-								</h3>
-							</div>
-							<p>Stay informed of new listings.</p>
-						</Col>
-						<Col lg={4} md={6}>
-							<SubscribeForm />
-						</Col>
-						<BusinessItem />
-						<BusinessItem />
-						<BusinessItem />
+				<Row>
+					<Col lg={8} md={6}>
+						<div className="d-flex align-items-center">
+						<h3 className="text-decoration-none text-secondary">
+							Recommended For You
+						</h3>
+						</div>
+						<p>Stay informed of new listings.</p>
+					</Col>
+					<Col lg={4} md={6}>
+						<SubscribeForm />
+					</Col>
+					<BusinessItem
+						title="Y's Boba Shop"
+						address="1234 This St, This City, TS 01010"
+						price="$732,000"
+						image="./images/Y's Boba Shop.jpg"
+					/>
+					<BusinessItem
+						title="Y's Commercial Building"
+						address="4321 This St, This City, TS 10101"
+						price="$1,370,000"
+						image="./images/Y's Building.jpg"
+					/>
+					<BusinessItem
+						title="Y's Rental Office Space"
+						address="1324 This St, This City, TS 00100"
+						price="$80,085"
+						image="./images/Y's Rental Office Space.jpg"
+					/>
 					</Row>
 				</div>
 			</div>
-
 			<Footer />
 		</div>
 	);
--- src/shared/components/BusinessItem.js
+++ src/shared/components/BusinessItem.js
@@ -1,29 +1,34 @@
-function BusinessItem() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+
+function BusinessItem(props) {
 	return (
 		<div className="col-sm-4">
 			<div className="card text-bg-light">
 				<img
-					src={"./images/Y's Boba Shop.jpg"}
+					src={props.image}
 					className="img-fluid"
-					alt="Example image"
+					alt={props.title}
 				/>
 				<div className="card-body">
 					<a href="#" className="stretched-link"></a>
 					<div className="row align-items-center text-left">
 						<div className="col-12">
-							<div className="d-flex align-items-center">
-								<h3 className="text-decoration-none text-secondary">
-									Fried Chicken Restaurant
-								</h3>
-								<button className="btn btn-outline-primary ml-auto">
-									<i className="bi bi-heart"></i>
-								</button>
-							</div>
-							<a className="text-decoration-none text-secondary">
-								4321 That St, That City, TS 01010
-							</a>
-							<br />
-							<a className="text-decoration-none text-primary">$567,000</a>
+						<div className="d-flex align-items-center">
+							<h3 className="text-decoration-none text-secondary">
+								{props.title}
+							</h3>
+							<button className="btn btn-outline-primary btn-heart border-0">
+								<FontAwesomeIcon icon={faHeart} />
+							</button>
+						</div>
+						<a className="text-decoration-none text-secondary">
+							{props.address}
+						</a>
+						<br />
+						<a className="text-decoration-none text-primary">
+							{props.price}
+						</a>
 						</div>
 					</div>
 				</div>
